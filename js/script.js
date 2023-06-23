7e5a6ef85d2aa2f3cac9fe49e01fba3bfe00d793
JLPacs <109599600+JLPacs@users.noreply.github.com>
2023-06-23 17:21:41 +0800

    working with the buttons

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
+let color = 'black';
+
 document.addEventListener("DOMContentLoaded", function() {
-    createBoard(32);
-})
+    createBoard(16);
+
+    let btn_popup = document.querySelector('#popup');
+    btn_popup.addEventListener('click', function () {   
+        let size = getSize();
+        createBoard(size);
+    });
+});
 
 function createBoard(size) {
     let board = document.querySelector('.board');
@@ -9,11 +17,43 @@ function createBoard(size) {
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
     let numDivs = size * size;
-
+    
     for(let i = 0; i < numDivs; i++) {
         let div = document.createElement('div');
-        div.style.backgroundColor = "yellow";
+        div.addEventListener('mouseover', colorDiv)
         board.insertAdjacentElement("beforeend", div);
     }
 
+}
+
+function getSize() {
+    let input = parseInt(prompt("Please input canvas size"));
+    let message = document.querySelector('#message');
+    console.log('1', input);
+    if(input === "" || isNaN(input) || input === null) {        
+        message.textContent = "Please provide a number";
+    } else if (input < 0 || input > 100) {
+        message.textContent = "Please provide a number between 0 - 100";
+    } else {
+        console.log('2', input);
+        message.textContent = "Approved!"
+        return input;
+    }
+}
+
+function colorDiv () {
+    if(color === "random") {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+        this.style.backgroundColor = 'black';
+    }
+}
+
+function setColor(colorChoice) {
+    color = colorChoice;
+}
+
+function resetBoard() {
+    let divs = document.querySelectorAll('div');
+        divs.forEach((div) => div.style.backgroundColor = 'white');
 }
